86a5ae684269425a4063a4cb35fcad493add13e8
caothanhphatit <71224037+caothanhphatit@users.noreply.github.com>
2022-05-16 16:58:02 +0700

    Add files via upload
    first commit

---
+++ src/main/java/tdtu/example/ecommercemanagement/EcommerceManagementApplication.java
@@ -0,0 +1,12 @@
+package tdtu.example.ecommercemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class EcommerceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceManagementApplication.class, args);
+    }
+
+}
